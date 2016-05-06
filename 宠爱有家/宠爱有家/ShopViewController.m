5299d7bb396b1c3a042d3176a386b0ff1b5fd572
刘鑫 <lx950217@163.com>
2016-05-06 11:27:22 +0800

    红色

--- 宠爱有家/宠爱有家/ShopViewController.m
+++ 宠爱有家/宠爱有家/ShopViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.title = @"商城";
-    self.view.backgroundColor = [UIColor yellowColor];
+    self.view.backgroundColor = [UIColor redColor];
     
 }
 
